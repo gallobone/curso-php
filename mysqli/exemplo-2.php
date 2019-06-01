0fc14b2058ee424e0609347d8a8d20e7782025aa
Fernando Orsi <fernando.orsi@infobase.com.br>
2019-06-01 15:54:28 -0300

    including mysqli mod

---
+++ mysqli/exemplo-2.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "root2019@Maldini33", "dbphp7");
+
+if($conn->connect_error){
+	echo "Error: ". $conn->connect_error;
+}
+
+$result = $conn->query("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$data = array();
+//PARAM MYSQLI_ASSOC , TRAZ SO OS NOMES DAS STRINGS
+while($row = $result->fetch_array(MYSQLI_ASSOC)){
+	
+	//var_dump($row);
+	array_push($data, $row);
+
+}
+
+
+echo json_encode($data);
+
+
+
+?>
