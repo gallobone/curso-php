414d1424f74738d45c7cedd83ca26b697b9a34f6
gallobone <orsi.f84@gmail.com>
2019-10-20 23:16:23 -0300

    criando a classe usuario e criando a conversa entre a classe SQL e a classe Usuario

--- DAO-v2/index.php
+++ DAO-v2/index.php
@@ -2,11 +2,21 @@
 
 require_once("config.php");
 
+/*comentei a forma antiga aqui nesse caso, pq agora estou chamando a classe Usuario, e la ja consta o select que precisa
+ser feito na tb_usuario
+
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+
+$root->loadById(7);
+
+echo $root;
 
 
 ?>
