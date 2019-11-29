5856ba5cddf2e92bf1ae802f11bf6bc6a7554874
gallobone <orsi.f84@gmail.com>
2019-11-29 01:46:53 -0300

    add comentarios a explicacao de como manipular diretorios

--- DIR/exemplo-2.php
+++ DIR/exemplo-2.php
@@ -2,8 +2,8 @@
 
 
 /*
-Abaixo, usamos a function scandir, que scaneia, verifica o que temos dentro do diret´´orio desejado.
-Esta functio retorna um array();
+Abaixo, usamos a function scandir, que scaneia, verifica o que temos dentro do diretorio desejado.
+Esta function retorna um array();
 */
 $images = scandir("images");
 
@@ -13,14 +13,27 @@ $data = array();
 
 foreach ($images as $img) {
 	
+	/*Abaixo , usamos a function de veririfição 'in_array', que retorna boleano. No caso ela recebe dois parametros, o "needle", 
+	que é onde voce deseja procurar, e um segundo parametro haystack, que é o que voce deseja procurar no primeiro parametro.
+	Melhor explicando: procure no needle $img o haystack ".", ".."
+	
+	Contudo, como retorna um booleano, fizemos uma validção para ver se retorna FALSE, portanto iniciamos com o sinal de exclamação !in_array()
+	*/
 	if(!in_array($img, array(".",".."))){
 		
+
+		//a varivel $filename retorna o path completo do arquivo nesse caso. Ex: images/nomedaimagem.jpg
 		$filename = "images".DIRECTORY_SEPARATOR . $img;
 
+		/*Abaixo usamos mais uma function dentro da variavel $info. A function pathinfo, que retorna informações do arquivo, como extensao,
+		nome do diretorio em que se encontra, nome base do arquivo, nome do arquivo sem extensao*/
 		$info = pathinfo($filename);
 
+		//abaixo adicionamos à variavel $info, a function filesize, que retorna o tamnho do arquivo
 		$info["size"] = filesize($filename);
+		//abaixo add à variavel $info, a function filetime, que retorna a data de criacao do arquivo, convertida dentro de um DateTime()
 		$info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+		
 		$info["url"] = "http://localhost/dev/curso-php/DIR/".str_replace("\\", "/", $filename);
 		//var_dump($info);
 
