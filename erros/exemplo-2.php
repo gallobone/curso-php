ea65dd570f89c3ee8a4cc8c4f68c65b80ca8f27d
Fernando Orsi <fernando.orsi@infobase.com.br>
2020-02-18 00:52:19 -0300

    Add exemplo 2, aula de como usar o metodo error_reporting

--- erros/exemplo-2.php
+++ erros/exemplo-2.php
@@ -1,6 +1,26 @@
 <?php
 
 
+//Function error_reporting() usada para manipular a exibição dos erros que voce quer que sejam ou nao exibidos.
+
+//E_ALL constante que retorna todos os Errors
+
+//E_NOTICE constante que retorna todos os NOTICES de erros.
+
+//E_WARNING constante que retorna todos os Warnings 
+
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+
+//abaixo podemos forçar ou nao um erro.
+//Caso a página NÃO receba o parametro NOME por Querystring, o php vai retornar um NOTICE
+$nome = $_GET["nome"];
+
+echo $nome;
+
+//Portanto, lembramos que usando o error_reporting, podemos configurar se queremos ou nao exbir , por exemplo, o NOTICE.
+
 
 
 ?>
